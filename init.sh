02d60fb73ded4abeb54e141d8246b6518f92654d
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-03-11 18:36:09 -0400

    starting a centralized config repo

---
+++ init.sh
@@ -0,0 +1,10 @@
+ln -s ~/.jazzyConfigs/zshrc ~/.zshrc
+ln -s ~/.jazzyConfigs/eslintrc ~/.eslintrc
+ln -s ~/.jazzyConfigs/prettierrc ~/.prettierrc
+ln -s ~/.jazzyConfigs/psqlrc ~/.psqlrc
+ln -s ~/.jazzyConfigs/stylelintrc ~/.stylelintrc
+
+# .config folder
+
+ln -s ~/.jazzyConfigs/kitty ~/.config/kitty
+ln -s ~/.jazzyConfigs/fontconfig ~/.config/fontconfig
