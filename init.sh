a27b4a6a65e6b869e8c1b93a6a695e837f3f0a3e
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-04-18 11:58:59 -0400

    tweaking init script

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-ln -s ~/.jazzyConfigs/zshrc ~/.zshrc
 ln -s ~/.jazzyConfigs/eslintrc ~/.eslintrc
 ln -s ~/.jazzyConfigs/prettierrc ~/.prettierrc
 ln -s ~/.jazzyConfigs/psqlrc ~/.psqlrc
@@ -13,3 +12,6 @@ ln -s ~/.jazzyConfigs/fontconfig ~/.config/fontconfig
 
 sudo pacman -S pkgfile zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm ~/.zshrc
+ln -s ~/.jazzyConfigs/zshrc ~/.zshrc
